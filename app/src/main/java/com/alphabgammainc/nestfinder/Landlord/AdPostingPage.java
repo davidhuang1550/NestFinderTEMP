ff9395e8222f3cddbfa75a75599ccf3728c9ea35
Soutrik Barua <sbarua91@gmail.com>
2017-05-06 18:30:27 -0400

    Updated the ad posting page

--- app/src/main/java/com/alphabgammainc/nestfinder/Landlord/AdPostingPage.java
+++ app/src/main/java/com/alphabgammainc/nestfinder/Landlord/AdPostingPage.java
@@ -69,67 +69,76 @@ public class AdPostingPage extends Fragment{
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mActivity =getActivity();
-        /**
-         * Get the respective views
-         */
-        adTitle =(EditText)getView().findViewById(R.id.title);
-        streetNumber=(EditText)getView().findViewById(R.id.streetNumber);
-        streetName=(EditText)getView().findViewById(R.id.streetName);
-        postalCode=(EditText)getView().findViewById(R.id.postalCode);
-        city=(EditText)getView().findViewById(R.id.city);
-        country=(EditText)getView().findViewById(R.id.country);
-        province =(EditText)getView().findViewById(R.id.province);
-        bedRooms=(EditText)getView().findViewById(R.id.bedrooms);
-        bathRooms=(EditText)getView().findViewById(R.id.bathrooms);
-        isFurnished=(CheckBox) getView().findViewById(R.id.furnished);
-        pets=(CheckBox)getView().findViewById(R.id.pets);
-
-        /**
-         * Convert the fetched data to be passed in the Locations object
-         */
-        madTitle= adTitle.toString();
-        mstreetName=streetName.toString();
-        mstreetNumber=streetNumber.toString();
-        mpostalCode = postalCode.toString();
-        mCity =city.toString();
-        mCountry=country.toString();
-        mProvince=province.toString();
-
-        /**
-         * Additional information about the apartment
-         */
-
-        mbedRooms =Integer.parseInt(bedRooms.toString());
-        mbathRooms = Integer.parseInt(bathRooms.toString());
-        misFurnished=Boolean.valueOf(isFurnished.toString());
-        mPets =Boolean.valueOf(pets.toString());
-
-        /**
-         * send the data into an address object
-         */
-
-        mAddress =new Address(mstreetNumber,mstreetName,mCity,mProvince,mpostalCode,mCountry);
-
-       // adLocation = new Location() instantiate the new location here
-
-        /**
-         * Submit button;
-         */
-        postAd =(Button)getView().findViewById(R.id.postAd);
-
-        postAd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                post_ad_to_database();
-            }
-        });
-
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-            mView = inflater.inflate(R.layout.ad_posting_page,container,false);
+        mView = inflater.inflate(R.layout.ad_posting_page,container,false);
+
+        /**
+         * Get the respective views
+         */
+
+        adTitle =(EditText)mView.findViewById(R.id.title);
+        streetNumber=(EditText)mView.findViewById(R.id.streetNumber);
+        streetName=(EditText)mView.findViewById(R.id.streetName);
+        postalCode=(EditText)mView.findViewById(R.id.postalCode);
+        city=(EditText)mView.findViewById(R.id.city);
+        country=(EditText)mView.findViewById(R.id.country);
+        province =(EditText)mView.findViewById(R.id.province);
+        bedRooms=(EditText)mView.findViewById(R.id.bedrooms);
+        bathRooms=(EditText)mView.findViewById(R.id.bathrooms);
+        isFurnished=(CheckBox)mView.findViewById(R.id.furnished);
+        pets=(CheckBox)mView.findViewById(R.id.pets);
+
+
+
+        postAd =(Button)mView.findViewById(R.id.postAd);
+
+
+
+        // adLocation = new Location() instantiate the new location here
+
+        /**
+         * Submit button;
+         */
+
+
+        postAd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                /**
+                 * Convert the fetched data to be passed in the Locations object
+                 */
+                madTitle= adTitle.getText().toString();
+                mstreetName=streetName.getText().toString();
+                mstreetNumber=streetNumber.getText().toString();
+                mpostalCode = postalCode.getText().toString();
+                mCity =city.getText().toString();
+                mCountry=country.getText().toString();
+                mProvince=province.getText().toString();
+
+                /**
+                 * Additional information about the apartment
+                 */
+
+                mbedRooms =Integer.parseInt(bedRooms.getText().toString());
+                mbathRooms = Integer.parseInt(bathRooms.getText().toString());
+                misFurnished=Boolean.valueOf(isFurnished.getText().toString());
+                mPets =Boolean.valueOf(pets.getText().toString());
+
+                /**
+                 * send the data into an address object
+                 */
+
+                mAddress =new Address(mstreetNumber,mstreetName,mCity,mProvince,mpostalCode,mCountry);
+
+                post_ad_to_database();
+            }
+        });
         return mView;
     }
 
